d2548c0becedd523de59f303b91b363116f58dd1
WeRockStar <kotchaphan.m@gmail.com>
2019-07-18 21:58:49 +0700

    Change declaration on viewmodel

--- app/src/main/java/com/werockstar/withkoin/di/AppModule.kt
+++ app/src/main/java/com/werockstar/withkoin/di/AppModule.kt
@@ -5,12 +5,13 @@ import com.werockstar.withkoin.core.CoroutinesDispatcher
 import com.werockstar.withkoin.core.ThreadDispatcher
 import com.werockstar.withkoin.ui.user.usecase.GetUsersUseCase
 import com.werockstar.withkoin.ui.user.viewmodel.UserViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val appModule = module {
     single { Gson() }
     factory { GetUsersUseCase(get(named("database")), get(named("remote"))) }
-    factory { UserViewModel(get()) }
+    viewModel { UserViewModel(get()) }
     single<ThreadDispatcher> { CoroutinesDispatcher() }
 }
--- app/src/main/java/com/werockstar/withkoin/di/DataModule.kt
+++ app/src/main/java/com/werockstar/withkoin/di/DataModule.kt
@@ -2,17 +2,17 @@ package com.werockstar.withkoin.di
 
 import androidx.room.Room
 import com.werockstar.withkoin.data.database.AppDatabase
-import com.werockstar.withkoin.data.repository.RoomGithubRepository
-import com.werockstar.withkoin.data.repository.RemoteGithubRepository
 import com.werockstar.withkoin.data.repository.GithubUserRepository
+import com.werockstar.withkoin.data.repository.RemoteGithubRepository
+import com.werockstar.withkoin.data.repository.RoomGithubRepository
 import org.koin.android.ext.koin.androidApplication
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val dataModule = module {
-    factory<GithubUserRepository>(named("database")) { RoomGithubRepository(get(), get()) }
-    factory<GithubUserRepository>(named("remote")) { RemoteGithubRepository(get(), get()) }
-    factory {
+    single<GithubUserRepository>(named("database")) { RoomGithubRepository(get(), get()) }
+    single<GithubUserRepository>(named("remote")) { RemoteGithubRepository(get(), get()) }
+    single {
         Room.databaseBuilder(
             androidApplication(), AppDatabase::class.java, "koin-app"
         ).build()
