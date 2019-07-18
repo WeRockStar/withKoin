63d2b4369685b24a31f08095a2d58327b1354f12
WeRockStar <kotchaphan.m@gmail.com>
2019-07-18 13:54:00 +0700

    Prepare fragment

---
+++ app/src/main/java/com/werockstar/withkoin/ui/user/UserDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.werockstar.withkoin.ui.user
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.werockstar.withkoin.R
+
+class UserDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user_detail, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/werockstar/withkoin/ui/user/UserFragment.kt
@@ -0,0 +1,22 @@
+package com.werockstar.withkoin.ui.user
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.werockstar.withkoin.R
+
+class UserFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+
+}
