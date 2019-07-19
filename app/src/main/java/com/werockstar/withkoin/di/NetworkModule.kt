186c92a1bfb122f281cad20e4d635c8146b880f0
WeRockStar <kotchaphan.m@gmail.com>
2019-07-19 10:04:18 +0700

    Enable cache on okHttp

--- app/src/main/java/com/werockstar/withkoin/di/NetworkModule.kt
+++ app/src/main/java/com/werockstar/withkoin/di/NetworkModule.kt
@@ -1,21 +1,25 @@
 package com.werockstar.withkoin.di
 
 import com.werockstar.withkoin.data.remote.GithubAPI
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
-        /**
-         * TODO: Caching enable
-         */
+        val cacheSize: Long = 10 * 1024 * 1024
+        val httpCacheDirectory = File(androidApplication().cacheDir, "http-cache")
+        val cache = Cache(httpCacheDirectory, cacheSize)
         OkHttpClient.Builder()
             .readTimeout(60, TimeUnit.SECONDS)
             .writeTimeout(60, TimeUnit.SECONDS)
+            .cache(cache)
             .addInterceptor(get<HttpLoggingInterceptor>())
             .build()
     }
