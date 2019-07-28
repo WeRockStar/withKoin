1e86d15aaf0f1ef94b9036fc3637a0493c87d18e
WeRockStar <kotchaphan.m@gmail.com>
2019-07-28 16:07:24 +0700

    Update appcompat lib

--- app/src/main/java/com/werockstar/withkoin/ui/user/UsersActivity.kt
+++ app/src/main/java/com/werockstar/withkoin/ui/user/UsersActivity.kt
@@ -1,13 +1,6 @@
 package com.werockstar.withkoin.ui.user
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.werockstar.withkoin.R
 
-class UsersActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
-    }
-}
+class UsersActivity : AppCompatActivity(R.layout.activity_user)
